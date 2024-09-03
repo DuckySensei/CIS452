0c30434750176ee7cf6a131f1d3ac5a6b9543cfe
Dylan M Toomey <123576887+DuckySensei@users.noreply.github.com>
2024-09-03 12:16:37 -0400

    Fixed Lab1Fixed.c Had a second memory issue

--- Lab1Fixed.c
+++ Lab1Fixed.c
@@ -13,15 +13,15 @@ int main() {
         printf("Please input your eos username: ");
         scanf("%s", data1);
         if (!strcmp(data1, "quit")) {
-            free(data1);  // Free data1 before breaking out of the loop
+            free(data1);  // Free up data when breaking out of the loop, should solve other memory issue
             break;
         }
         data2 = malloc(SIZE);
         for (i = 0; i < SIZE; i++)
             data2[i] = data1[i];
-        free(data1);  // Free data1 after it's no longer needed
+        free(data1); 
         printf("data2 :%s:\n", data2);
-        free(data2);  // Free data2 to prevent memory leak
+        free(data2);  // Free data2 to prevent that sneaky little memory leak
     } while (1);
 
     return 0;
